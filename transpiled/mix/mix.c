88fe5666c2eee993d6e95775f0d54b378d0900d9
BrownNPC <deaddogfuneral@gmail.com>
2026-07-06 10:52:52 +0500

    fix network bugs

--- transpiled/mix/mix.c
+++
@@ -1,3 +0,0 @@
-#include "mix.h"
-
-// -- Implementation --
--- transpiled/mix/mix.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "so/builtin/builtin.h"
-#include <SDL3_mixer/SDL_mixer.h>
-#include "sdl/sdl.h"
-
-// -- Types --
-
-typedef void (*mix_TrackStoppedCallback)(void*, MIX_Track*);
